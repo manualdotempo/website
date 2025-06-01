e97e24d4ae330525e63142fcbf9666cd95e91eda
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-06-01 18:09:22 -0300

    Finalização parcial do header e inicialização da estilização de quemsomos.html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,13 @@ const botaoMenu = document.querySelector('.abrir-menu')
 const menu = document.querySelector('.navegador-mobile')
 menu.style.display = 'none'
 
-botaoMenu.addEventListener('click', function() {    
+document.querySelectorAll('.navegador-paginas a, .navegador-mobile a').forEach(link => {
+    if (link.pathname.split('/').pop() == window.location.pathname.split('/').pop()) {
+        link.classList.add('link-ativo')
+    }
+})
+
+botaoMenu.addEventListener('click', function () {
     if (menu.style.display == 'none') {
         menu.style.display = 'block'
         botaoMenu.style.content = 'url(../assets/images/icone-fechar.png)'
