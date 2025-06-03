887430276702920f42bd3db19340935fb6de874e
FelipeFerrete <felipeferretelemes@gmail.com>
2025-06-02 22:34:26 -0300

    Add: API clima + html, css, JavaScript e image

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,94 @@
-// Abrir e fechar menu header mobile
-const botaoMenu = document.querySelector('.abrir-menu')
-const menu = document.querySelector('.navegador-mobile')
-menu.style.display = 'none'
+document.querySelector('#search').addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-botaoMenu.addEventListener('click', function () {
-    if (menu.style.display == 'none') {
-        menu.style.display = 'block'
-        botaoMenu.style.content = 'url(../assets/images/icone-fechar.png)'
-        document.body.style.position = 'fixed'
+    const cityName = document.querySelector('#city_name').value;
+
+    if (!cityName) {
+        document.querySelector("#weather").classList.remove('show');
+        showAlert('Você precisa digitar uma cidade...');
+        return;
+    }
+
+    const apiKey = 'cd7dadee4dfaa5653ccf17ea48cc6223';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`
+
+    const results = await fetch(apiUrl);
+    const json = await results.json();
+
+    if (json.cod === 200) {
+        showInfo({
+            city: json.name,
+            country: json.sys.country,
+            temp: json.main.temp,
+            tempMax: json.main.temp_max,
+            tempMin: json.main.temp_min,
+            description: json.weather[0].description,
+            tempIcon: json.weather[0].icon,
+            windSpeed: json.wind.speed,
+            humidity: json.main.humidity,
+        });
     } else {
-        menu.style.display = 'none'
-        botaoMenu.style.content = 'none'
-        document.body.style.position = 'relative'
-    }
-})
+        document.querySelector("#weather").classList.remove('show');
+        showAlert(`
+            Não foi possível localizar...
 
-// Estilizar header conforme url da página
-document.querySelectorAll('.navegador-paginas a, .navegador-mobile a').forEach(link => {
-    if (link.pathname.split('/').pop() == window.location.pathname.split('/').pop()) {
-        link.classList.add('link-ativo')
+            <img src="./assets/images/void.png"/>
+        `)
     }
-})
+});
 
-// Abrir e fechar faq
-document.querySelectorAll('.perguntas li').forEach(pergunta => {
-    pergunta.addEventListener('click', function () {
-        pergunta.querySelector('.resposta').classList.toggle('ativa')
-        pergunta.querySelector('img').classList.toggle('rodar')
-    })
-})
+function showInfo(json){
+    showAlert('');
+
+    document.querySelector("#weather").classList.add('show');
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`;
+
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_description').innerHTML = `${json.description}`;
+    document.querySelector('#temp_img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#humidity').innerHTML = `${json.humidity}%`;
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}km/h`;
+}
+
+function showAlert(msg) {
+    document.querySelector('#alert').innerHTML = msg;
+}
+
+
+
+
+// // Abrir e fechar menu header mobile
+// const botaoMenu = document.querySelector('.abrir-menu')
+// const menu = document.querySelector('.navegador-mobile')
+// menu.style.display = 'none'
+
+// botaoMenu.addEventListener('click', function () {
+//     if (menu.style.display == 'none') {
+//         menu.style.display = 'block'
+//         botaoMenu.style.content = 'url(../assets/images/icone-fechar.png)'
+//         document.body.style.position = 'fixed'
+//     } else {
+//         menu.style.display = 'none'
+//         botaoMenu.style.content = 'none'
+//         document.body.style.position = 'relative'
+//     }
+// })
+
+// // Estilizar header conforme url da página
+// document.querySelectorAll('.navegador-paginas a, .navegador-mobile a').forEach(link => {
+//     if (link.pathname.split('/').pop() == window.location.pathname.split('/').pop()) {
+//         link.classList.add('link-ativo')
+//     }
+// })
+
+// // Abrir e fechar faq
+// document.querySelectorAll('.perguntas li').forEach(pergunta => {
+//     pergunta.addEventListener('click', function () {
+//         pergunta.querySelector('.resposta').classList.toggle('ativa')
+//         pergunta.querySelector('img').classList.toggle('rodar')
+//     })
+// })
